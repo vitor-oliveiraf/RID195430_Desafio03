5af365baa443a718b1bba9bb30674a2d3aa05ff4
Vitor de Oliveira <vitoro.fabiano@gmail.com>
2024-12-27 00:26:29 -0300

    feat: implement task creation and clear functionality

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let tasks = [
   {
+    id: 1,
     description: "Implementar tela de listagem de tarefas",
     label: "frontend",
     CreatedAt: "21/08/2024",
     concluded: false,
   },
   {
+    id: 2,
     description: "Criar endpoint para cadastro de tarefas",
     label: "backend",
     CreatedAt: "22/08/2024",
-    concluded: true,
+    concluded: false,
   },
   {
+    id: 3,
     description: "Implementar protótipo da listagem de tarefas",
     label: "ux",
     CreatedAt: "23/08/2024",
@@ -19,15 +22,12 @@ let tasks = [
   },
 ];
 
-// window.onload renderiza tudo dentro dele ao carregar a página
-window.onload = () => {
+// Criar componente de tarefa
+const createTaskComponent = (task) => {
   const sectionList = document.getElementById("sectionList");
-
-  // forEach percorre o array de tasks e cria um card para cada uma
-  tasks.forEach((task) => {
-    const cardContainer = document.createElement("div");
-    cardContainer.className = "cardContainer";
-    cardContainer.innerHTML = `
+  const cardContainer = document.createElement("div");
+  cardContainer.className = "cardContainer";
+  cardContainer.innerHTML = `
             <div class="cardInfo">
                 <p class=${
                   !task.concluded ? "description" : "descriptionConcluido"
@@ -38,21 +38,82 @@ window.onload = () => {
                 </div>
             </div>
             <button class=${!task.concluded ? "btnConcluir" : "btnConcluido"}>${
-      !task.concluded
-        ? "Concluir"
-        : "<span class='material-symbols-outlined'>check</span>"
-    }</button>
+    !task.concluded
+      ? "Concluir"
+      : "<span class='material-symbols-outlined'>check</span>"
+  }</button>
         `;
-    sectionList.appendChild(cardContainer);
+
+  // Concluir tarefa
+  const button = cardContainer.querySelector("button");
+  button.addEventListener("click", () => {
+    task.concluded = !task.concluded;
+    sectionList.innerHTML = ""; // Limpa a lista
+    tasks.forEach((task) => createTaskComponent(task)); // Re-renderiza tarefas
+    updatecounter(); // Atualiza o contador
   });
 
-  // adicionar o contador de tarefas concluídas
-  const totalTasks = tasks.length;
-  const totalTasksConcluded = tasks.filter((task) => task.concluded).length;
-  const sectionCount = document.getElementById("sectionCount");
-  const textCounter = document.createElement("p");
-  textCounter.className = "taskCounter";
-  textCounter.innerText = `${totalTasksConcluded} de ${totalTasks} tarefas concluídas`;
+  sectionList.appendChild(cardContainer);
 
-  sectionCount.appendChild(textCounter);
+  sectionList.appendChild(cardContainer);
+};
+
+// Contador de tarefas concluídas
+function updatecounter() {
+  const taskConcluded = tasks.filter((task) => task.concluded).length;
+  const taskCountElement = document.getElementById("taskCount");
+  taskCountElement.innerText = `${taskConcluded} de ${tasks.length} tarefas`;
+}
+
+// Criar nova tarefa
+const createTask = (event) => {
+  event.preventDefault();
+  const inputDescription = document.getElementById("inputDescription");
+  const inputLabel = document.getElementById("inputLabel");
+  const newTask = {
+    id: tasks.length + 1,
+    description: inputDescription.value,
+    label: inputLabel.value,
+    CreatedAt: new Date().toLocaleDateString(),
+    concluded: false,
+  };
+
+  inputDescription.value = "";
+  inputLabel.value = "";
+
+  createTaskComponent(newTask);
+
+  tasks = [...tasks, newTask];
+
+  updatecounter();
+};
+
+// Função para apagar tarefas concluídas
+function clearCompletedTasks() {
+  // Filtra apenas as tarefas não concluídas
+  tasks = tasks.filter((task) => !task.concluded);
+
+  // Atualiza a exibição no DOM
+  const sectionList = document.getElementById("sectionList");
+  sectionList.innerHTML = ""; // Limpa todas as tarefas
+  tasks.forEach((task) => createTaskComponent(task)); // Re-renderiza tarefas
+
+  // Atualiza o contador
+  updatecounter();
+}
+
+// window.onload renderiza tudo dentro dele ao carregar a página
+window.onload = () => {
+  const form = document.getElementById("formTodo");
+  form.addEventListener("submit", createTask);
+
+  // forEach percorre o array de tasks e cria um card para cada uma
+  tasks.forEach((task) => {
+    createTaskComponent(task);
+  });
+  updatecounter();
+
+  // Event listener para apagar concluídas
+  const clearCompletedButton = document.getElementById("clearCompleted");
+  clearCompletedButton.addEventListener("click", clearCompletedTasks);
 };
